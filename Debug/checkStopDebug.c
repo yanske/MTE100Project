4e62a6d99b42b17680eba537bafb660540acbfc0
Yan Ke <yansongke1@gmail.com>
2016-11-18 13:08:20 -0500

    checkstopdebug

---
+++ Debug/checkStopDebug.c
@@ -0,0 +1,16 @@
+bool checkStop(bool shotsReached, int range)
+{
+	const int MAX_RANGE = range;
+	if(SensorValue[S1] < MAX_RANGE || shotsReached)
+		return true; //ie stop
+	else 
+		return false;
+}
+
+task main()
+{
+	if(checkStop(false, 50))
+	{
+		displayString(0, "Found.");
+	}
+}
