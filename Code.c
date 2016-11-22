a6e9db2821d3ab2f8a8bb152975b01c30bc6e58c
Stratzeh <jonathan.chung021@hotmail.com>
2016-11-22 13:46:46 -0500

    Update Code.c

--- Code.c
+++ Code.c
@@ -3,25 +3,25 @@ void rotate(bool left, float time, int encoder)
 {
 	float halfTime = time / 2, randomTime = 0;
 	int power = 5;
-
+	
 	if(left)
-		power = 5;
-
+		power = 10;
+		
 	else
-		power = -5;
-
-	if(time < 0) 	// User setting location manually
+		power = -10;
+	
+	if(time < 0) 							// User setting location manually
 	{
 		motor[motorB] = power; 	// Need to check if this rotates left or right
-		wait1Msec(100);
+		wait1Msec(500);
 	}
-
+	
 	else	// Randomized
 	{
 		randomTime = random[halfTime];	// Select random time between 0 and halfTime
-		time1[T1] = 0;			// Reset timer
-		motor[motorB] = power;		// Begin moving shooting mechanism
-
+		time1[T1] = 0;									// Reset timer
+		motor[motorB] = power;					// Begin moving shooting mechanism
+		
 		while(time1[T1] < randomTime)
 		{
 		if(left)
@@ -30,7 +30,7 @@ void rotate(bool left, float time, int encoder)
 			while(nMotorEncoder[motorB] > encoder){}
 		}
 	}
-
+	
 	motor[motorB] = 0;
 }
 
@@ -39,24 +39,24 @@ float touchTimer()
 {
 	displayString(0, "Hold Touch Sensor for duration");
 	displayString(1, "of desired shot interval");
-
+	
 	float recordedTime = 0;
-
+	
 	while(SensorValue[S1] == 0) // Need to check sensor port
 	{
 	}
-
+	
 	time1[T1] = 0;
-
-	while(SensorValue[S1] == 1)
+	
+	while(SensorValue[S1] == 1) 
 	{
 	}
-
+	
 	recordedTime = time1[T1];
-
+	
 	eraseDisplay();
 	displayString(0, "Time Interval Set: %f", recordedTime);
-
+	
 	return recordedTime;
 }
 
@@ -64,28 +64,28 @@ float touchTimer()
 void setLocation(float time)
 {
 	/*
-	Assuming we already set the motor encoder
+	Assuming we already set the motor encoder 
 	Shooting mechanism pointing straight = motor encoder is 0
 	*/
-
-	float encCount = 0;		// Random encoder count
-	bool turn = true; 		// assuming going left is true
-
-	turn = random(1);		// Left or right? 1 = Left
-	encCount = random(45);		// Randomize encoder location
-
-	if(!turn)
-		encCount = -encCount;	// Set to negative count if turning right
+	
+	float encCount = 0;						// Random encoder count
+	bool turn = true; 						// assuming going left is true
+	
+	turn = random(1);							// Left or right? 1 = Left
+	encCount = random(45);				// Randomize encoder location
+	
+	if(!turn)	
+		encCount = -encCount;				// Set to negative count if turning right
 
 	rotate(turn, time, encCount);	// Send L/R, shot interval, encoder loaction
 }
 
 
 void loadBall(){
-	nMotorEncoder[motorA] = 0;
-	while (nMotorEncoder[motorA] < 360)
-		motor[motorA] = 20;
-	motor[motorA] = 0;
+	nMotorEncoder[motorC] = 0;
+	while (nMotorEncoder[motorC] > -360)
+		motor[motorC] = -50;
+	motor[motorC] = 0;
 }
 
 //=== YK Functions ==========================================
@@ -108,7 +108,7 @@ bool checkStop(bool shotsReached)
 	const int MAX_RANGE = 50;
 	if(SensorValue[S1] < MAX_RANGE || shotsReached)
 		return true; //ie stop
-	else
+	else 
 		return false;
 }
 
@@ -131,28 +131,46 @@ task main()
 	SensorType[S1] = sensorTouch;
 	SensorType[S2] = sensorSONAR;
 	float time = 0;
-
+	
 	bool randomShot = true, eStop = false; //emergency stop
-
+	
 	//Output Instructions
-
-	while(nNxtButtonPressed != 1 || nNxtButtonPressed != 2){}
-
+	
+	while(nNxtButtonPressed == -1 || nNxtButtonPressed == 3 || nNxtButtonPressed == 0){}
+	
+	//displayString(0, "DJAS");
+	//wait1Msec(10000);
+	
 	//Accept number of desired shots
-
+	
 	if(nNxtButtonPressed == 1)
 	{
 		//Output Instructions
 		while(nNxtButtonPressed != 3)
 		{
-			if(nNxtButtonPressed == 1)
-				rotate(true, -1, 0);
-			else
-				rotate(false, -1, 0);
+			while(nNxtButtonPressed == -1){}
+				if(nNxtButtonPressed == 1)
+				{
+					displayString(0, "PIECE OF CRAP");
+					rotate(true, -1, 0);
+				}
+				else if(nNxtButtonPressed == 2)
+				{
+					eraseDisplay();
+					displayString(0, "EVEN MORE CRAP");
+					rotate(false, -1, 0);
+				}
+			while(nNxtButtonPressed == -1){}
+			displayString(0, "WHAT THE CRAP");
 		}
 		randomShot = false;
+		displayString(9, "FALSE");
+		wait1Msec(1000);
 	}
-
+	
+	displayString(0, "Next");
+	wait1Msec(1000);
+	
 	while(nNxtButtonPressed != 3)
 	{
 		eraseDisplay();
@@ -160,17 +178,17 @@ task main()
 		displayString(1, "Press 3 to continue");
 		displayString(2, "Else press any other");
 		displayString(3, "button to reset time");
-
+		
 		while(nNxtButtonPressed == -1){}
 	}
-
+	
 	while(SensorValue[S2] > 100){}
 	time1[T1] = 0;
-
+	
 	if(randomShot)
 		setLocation(time);
-
-	loadBall();
-
-	//While time < interval block on flowchart
+	
+	loadBall();	
+	
+	//While time < interval location on flowchart
 }
