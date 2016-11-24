fcafb15ac96e0e54b63202ff9318bea301531a76
Yan Ke <yansongke1@gmail.com>
2016-11-24 18:35:37 -0500

    finalize code

--- Code.c
+++ Code.c
@@ -3,25 +3,25 @@ void rotate(bool left, float time, int encoder)
 {
 	float halfTime = time / 2, randomTime = 0;
 	int power = 5;
-	
+
 	if(left)
 		power = 10;
-		
+
 	else
 		power = -10;
-	
+
 	if(time < 0) 							// User setting location manually
 	{
 		motor[motorB] = power; 	// Need to check if this rotates left or right
 		wait1Msec(500);
 	}
-	
+
 	else	// Randomized
 	{
 		randomTime = random[halfTime];	// Select random time between 0 and halfTime
 		time1[T1] = 0;									// Reset timer
 		motor[motorB] = power;					// Begin moving shooting mechanism
-		
+
 		while(time1[T1] < randomTime)
 		{
 		if(left)
@@ -30,7 +30,7 @@ void rotate(bool left, float time, int encoder)
 			while(nMotorEncoder[motorB] > encoder){}
 		}
 	}
-	
+
 	motor[motorB] = 0;
 }
 
@@ -40,24 +40,25 @@ float touchTimer()
 	displayString(0, "Hold Touch Sensor");
 	displayString(1, "to set shot");
 	displayString(2, "interval");
-	
+
 	float recordedTime = 0;
-	
+
 	while(SensorValue[S1] == 0) // Need to check sensor port
 	{
 	}
-	
+
 	time1[T1] = 0;
-	
-	while(SensorValue[S1] == 1) 
+
+	while(SensorValue[S1] == 1)
 	{
 	}
-	
+
 	recordedTime = time1[T1];
-	
+
 	eraseDisplay();
-	displayString(0, "Time Interval Set: %f", recordedTime);
-	
+	displayString(0, "Time Interval");
+	displayString(1, "Set: %f", recordedTime);
+
 	return recordedTime;
 }
 
@@ -65,23 +66,23 @@ float touchTimer()
 void setLocation(float time)
 {
 	/*
-	Assuming we already set the motor encoder 
+	Assuming we already set the motor encoder
 	Shooting mechanism pointing straight = motor encoder is 0
 	*/
-	
+
 	float encCount = 0;						// Random encoder count
 	bool turn = false;
-	
+
 	int firstRandom = 0, secondRandom = 0;
 	firstRandom = random(3);
 	secondRandom = random(3);
-	
+
 	if(firstRandom == secondRandom)
 		turn = true;
 
 	encCount = random(25);				// Randomize encoder location
-	
-	if(!turn)	
+
+	if(!turn)
 		encCount = -encCount;				// Set to negative count if turning right
 
 	rotate(turn, time, encCount);	// Send L/R, shot interval, encoder loaction
@@ -114,12 +115,12 @@ void shootBall(float time, int pullBackValue, int power)
 	motor[motorA] = 0;
 }
 
-bool checkStop(bool shotsReached)
+bool checkStop(bool shotsLeft)
 {
-	const int MAX_RANGE = 50;
-	if(SensorValue[S1] < MAX_RANGE || shotsReached)
+	const int MAX_RANGE = 100;
+	if(SensorValue[S2] < MAX_RANGE || !shotsLeft)
 		return true; //ie stop
-	else 
+	else
 		return false;
 }
 
@@ -137,52 +138,73 @@ bool displayData(float time, int shotIndex, int total)
 	return shotsRemaining;
 }
 
-//=== MAIN =====================================
+//=== Button Hold / Release Function ====================
+void buttonHold(button)
+{
+	while(nNxtButtonPressed == -1){}
+	while(nNxtButtonPressed != -1){}
+}
+
+//=== MAIN ==============================================
 task main()
 {
+
+	
 	SensorType[S1] = sensorTouch;
 	SensorType[S2] = sensorSONAR;
 	float time = 0;
 	int totalShots = 0, shotsShot = 0;
-	
+
 	bool randomShot = true, eStop = false; //emergency stop
-	
+
 	displayString(0, "Set Location");
-	displayString(1, "Left - Random");
-	displayString(2, "Right - Manual");
-	
+	displayString(1, "Press left button");
+	displayString(2, "for random");
+	displayString(3, "locations");
+	displayString(5, "Press right button");
+	displayString(6, "to set manually");
+
 	while(nNxtButtonPressed == -1 || nNxtButtonPressed == 3 || nNxtButtonPressed == 0){}
-	
-	//Accept number of desired shots
-	
+
 	if(nNxtButtonPressed == 1)
 	{
-		//Output Instructions
+		
+		eraseDisplay();
+		displayString(0, "Manual Set");
+		displayString(1, "Press right button");
+		displayString(2, "to rotate right");
+		displayString(3, "Press left button");
+		displayString(4, "to rotate left");
+		displayString(6, "Press orange")
+		displayString(7, "button to go on");
+
+		while(nNxtButtonPressed == -1) {}
+
 		while(nNxtButtonPressed != 3)
 		{
 			while(nNxtButtonPressed == -1){}
-			
+
 			if(nNxtButtonPressed == 1)
 				rotate(false, -1, 0);
-		
+
 			else if(nNxtButtonPressed == 2)
 				rotate(true, -1, 0);
-		
+
 			while(nNxtButtonPressed == -1){}
 		}
 		randomShot = false;
 	}
-	
+
 	do
 	{
 		eraseDisplay();
 		time = touchTimer();
-		displayString(1, "Press 3 to go on");
-		displayString(2, "Else press any");
-		displayString(3, "other button");
-		displayString(4, "to set interval");
-		displayString(5, "again");
-		
+		displayString(3, "Press Orange");
+		displayString(4, "button to go on");
+		displayString(5, "Else press any");
+		displayString(6, "other button");
+		displayString(7, "to reset interval");
+
 		while(nNxtButtonPressed == -1){}
 	} while(nNxtButtonPressed != 3);
 
@@ -193,76 +215,91 @@ task main()
 	displayString(4, "-Hold to +-5");
 	displayString(5, "-Orange to enter");
 	displayString(6, "Total Shots: %d", totalShots);
-	
-	while(nNxtButtonPressed !=3)
-	{
+	wait1Msec(1000);
+
+	while (nNxtButtonPressed !=3){
+		while(nNxtButtonPressed == -1){}
 		if(nNxtButtonPressed == 1)
 		{
+			time1[T2] = 0;
 			while(nNxtButtonPressed == 1)
 			{
-				timer[T2] = 0;
-				if(timer[T2] > 500)
+
+				if(time1[T2] > 1000)
 				{
 					totalShots+=5;
+					displayString(6, "                         ");
 					displayString(6, "Total Shots: %d", totalShots);
+					time1[T2] = 0;
 				}
 			}
-			while(nNxtButtonPressed != 1)
+
+			if(time1[T2] <= 500)
 			{
-				if(timer[T2] <= 500)
-				{
-					totalShots++;
-					displayString(6, "Total Shots: %d", totalShots);
-				}
-			}	
+				totalShots++;
+				displayString(6, "                         ");
+				displayString(6, "Total Shots: %d", totalShots);
+			}
+
 		}
 
-		if(nNxtButtonPressed == 2)
+		else if(nNxtButtonPressed == 2)
 		{
-			while(nNxtButtonPressed == 2)
+			time1[T2] = 0;
+		while(nNxtButtonPressed == 2)
 			{
-				timer[T2] = 0;
-				if(timer[T2] > 500)
+
+				if(time1[T2] > 1000)
 				{
 					totalShots-=5;
+					displayString(6, "                         ");
 					displayString(6, "Total Shots: %d", totalShots);
+					time1[T2] = 0;
 				}
 			}
-			while(nNxtButtonPressed != 2)
+
+			if(time1[T2] <= 500)
 			{
-				if(timer[T2] <= 500)
-				{
-					totalShots--;
-					displayString(6, "Total Shots: %d", totalShots);
-				}
-			}	
+				totalShots--;
+				displayString(6, "                         ");
+				displayString(6, "Total Shots: %d", totalShots);
+			}
+
 		}
 	}
 	while(nNxtButtonPressed == 3){};
-	
+
 	eraseDisplay();
 	displayString(0, "Start by");
 	displayString(1, "putting racket");
 	displayString(2, "in front of");
 	displayString(3, "sonar sensor");
-	
+
 	while(SensorValue[S2] > 150){}
-	
-	while(eStop == false)
+
+	if(totalShots-shotsShot){
+
+	while(!eStop)
 	{
 		time1[T1] = 0;
-	
+
 		if(randomShot)
 			setLocation(time);
-		
-		loadBall();	
-		
+
+		loadBall();
+
 		while(time1[T1] < time - 200){}
-		
+
 		shootBall(0, -170, -60);
 		shotsShot++;
-		
+
 		eStop = displayData(time, shotsShot, totalShots);
-		eStop = checkStop(totalShots - shotsShot);
+		eStop = checkStop(totalShots-shotsShot);
+	}
+
+	while(nNxtButtonPressed == -1){}   //Let user see displayData stuff
+	eraseDisplay();
+	displayString(0, "Press any button");
+	displayString(1, "to end program");
 	}
 }
