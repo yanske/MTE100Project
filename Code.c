4447048370ade97c28f9907de8760f77be7433d4
Stratzeh <jonathan.chung021@hotmail.com>
2016-11-28 21:06:29 -0500

    Update Code.c

--- Code.c
+++ Code.c
@@ -1,4 +1,4 @@
-//=== JC Functions ======================================= i forgot to push...
+//=== Rotate Function - Written by Jonathan Chung ==============
 void rotate(bool left, float time, int encoder)
 {
 	float halfTime = time / 2, randomTime = 0;
@@ -10,31 +10,31 @@ void rotate(bool left, float time, int encoder)
 	else
 		power = -10;
 
-	if(time < 0) 							// User setting location manually
+	if(time < 0) 			// User setting location manually
 	{
-		motor[motorB] = power; 	// Need to check if this rotates left or right
-		wait1Msec(500);
+		motor[motorB] = power; 	// Rotate in direction user is indicating
+		wait1Msec(500);		
 	}
 
-	else	// Randomized
+	else		// Randomized location setting
 	{
 		randomTime = random[halfTime];	// Select random time between 0 and halfTime
-		time1[T1] = 0;									// Reset timer
-		motor[motorB] = power;					// Begin moving shooting mechanism
+		time1[T1] = 0;			// Reset timer
+		motor[motorB] = power;		// Begin moving shooting mechanism
 
 		while(time1[T1] < randomTime)
 		{
-		if(left)
-			while(nMotorEncoder[motorB] < encoder){}
-		else
-			while(nMotorEncoder[motorB] > encoder){}
+			if(left)
+				while(nMotorEncoder[motorB] < encoder){}
+			else
+				while(nMotorEncoder[motorB] > encoder){}
 		}
 	}
 
 	motor[motorB] = 0;
 }
 
-//Touch Sensor Timer Function
+//=== Timer Function - Written by Jonathan Chung ===============
 float touchTimer()
 {
 	displayString(0, "Hold Touch Sensor");
@@ -43,7 +43,7 @@ float touchTimer()
 
 	float recordedTime = 0;
 
-	while(SensorValue[S1] == 0) // Need to check sensor port
+	while(SensorValue[S1] == 0)
 	{
 	}
 
@@ -62,15 +62,15 @@ float touchTimer()
 	return recordedTime;
 }
 
-//=== NC Functions ================================
+//=== Randomized Location Function - Written by Naitao Cui ==============
 void setLocation(float time)
 {
 	/*
-	Assuming we already set the motor encoder
 	Shooting mechanism pointing straight = motor encoder is 0
+	Maximum Angle of Rotateion = 25 degrees on both sides
 	*/
 
-	float encCount = 0;						// Random encoder count
+	float encCount = 0;				// Variable for random encoder count
 	bool turn = false;
 
 	int firstRandom = 0, secondRandom = 0;
@@ -83,68 +83,67 @@ void setLocation(float time)
 	encCount = random(25);				// Randomize encoder location
 
 	if(!turn)
-		encCount = -encCount;				// Set to negative count if turning right
+		encCount = -encCount;			// Set to negative encoder count if turning right
 
-	rotate(turn, time, encCount);	// Send L/R, shot interval, encoder loaction
+	rotate(turn, time, encCount);	// Send L/R, shot interval, encoder loaction to rotate function
 }
 
-
+//=== Load Function - Written by Naitao Cui ==============
 void loadBall(){
 	nMotorEncoder[motorC] = 0;
-	while (nMotorEncoder[motorC] > -341)
+	while (nMotorEncoder[motorC] > -341)		// Encoder count for 1 rotation (Motor slightly off)
 		motor[motorC] = -42;
 	motor[motorC] = 0;
 }
 
-//=== YK Functions ==========================================
+//=== Shoot Function - Written by Yan Song Ke =============
 void shootBall(float time, int pullBackValue, int power)
 {
-	const int PULL_BACK = pullBackValue;
-	//time1[T1] = 0;
+	const int PULL_BACK = pullBackValue;		// Encoder count to pull shooter arm back
 	nMotorEncoder[motorA] = 0;
 	motor[motorA] = power;
 
-	while(nMotorEncoder[motorA]>PULL_BACK){}
+	while(nMotorEncoder[motorA] > PULL_BACK){}
 	motor[motorA] = 0;
 
-	//loadBall(); //edit to appropriate power
-	//while(time1[T1]< time){}
 	motor[motorA] = power;
 
-	while(nMotorEncoder[motorA]>-380){}  //test to see if there is a margine of error and a loop to set it to 360 is needed
+	while(nMotorEncoder[motorA] > -380){} 		// Reset arm back to original position
 	motor[motorA] = 0;
 }
 
+//=== Emergency Stop Function - Written by Yan Song Ke ==============
 bool checkStop(bool shotsLeft)
 {
-	const int MAX_RANGE = 100;
-	if(SensorValue[S2] < MAX_RANGE || !shotsLeft)
+	const int MAX_RANGE = 100;			// Max range to trigger sonar sensor
+	if(SensorValue[S2] < MAX_RANGE || !shotsLeft)	// If object in front of sensor or no more shots left
 		return true; //ie stop
 	else
 		return false;
 }
 
+//=== Button Held Down for Desired Shots Function - Written by Yan Song Ke ==============
 void buttonHold(bool right, int & totalShots)
 {
 		time1[T2] = 0;
-		bool debounce = false;
-		int buttonValue=1;
+		bool debounce = false;			// Boolean to jump 5 shots
+		int buttonValue = 1;
 		if(!right)
 		{
 			buttonValue =2;
 		}
-		while(nNxtButtonPressed == buttonValue)
+		while(nNxtButtonPressed == buttonValue)	// While button is being held down
 		{
-			if(time1[T2] > 900)
+			if(time1[T2] > 900)		// If button held for more than certain time
 			{
 				debounce = true;
 				if(right)
 				{
-					totalShots+=5;
+					totalShots+=5;	// +5 shots to "desired shots"
 				}
 				else
 				{
-					totalShots-=5;
+					totalShots-=5;	// -5 shots to "desired shots"
 				}
 
 				if(totalShots < 0)
@@ -168,7 +167,7 @@ void buttonHold(bool right, int & totalShots)
 					totalShots--;
 				}
 
-				if(totalShots<0)
+				if(totalShots < 0)
 				{
 					totalShots = 0;
 				}
@@ -177,18 +176,13 @@ void buttonHold(bool right, int & totalShots)
 		}
 }
 
-//=== SQ Functions ======================================
+//=== Display Data Function - Written by Sara Qi ==============
 void displayData(float time, int shotIndex, int total)
 {
 	eraseDisplay();
-	//bool shotsRemaining = false;
 	displayString(1, "Frequency: 1 ball/%.2fs", time/1000);
 	displayString(2, "Balls shot: %d", shotIndex);
 	displayString(3, "Remaining shots: %d", total - shotIndex);
-	//if(total - shotIndex > 0)
-		//shotsRemaining = true;
-
-	//return shotsRemaining;
 }
 
 //=== Button Hold / Release Function ====================
@@ -201,11 +195,12 @@ void buttonHold()
 //=== MAIN ==============================================
 task main()
 {
-	SensorType[S1] = sensorTouch;
+	SensorType[S1] = sensorTouch;		// Initialize sensors
 	SensorType[S2] = sensorSONAR;
-	float time = 0;
+	float time = 0;				// Shooting time interval (time between shots)
 	int totalShots = 0, shotsShot = 0;
-	bool randomShot = true, eStop = false; //emergency stop
+	bool randomShot = true, eStop = false;	// Random location boolean 
+						// and emergency stop boolean
 
 	displayString(0, "Set Location");
 	displayString(1, "Press left button");
@@ -232,7 +227,6 @@ task main()
 		while(nNxtButtonPressed != 3)
 		{
 			while(nNxtButtonPressed == -1){}
-			//buttonHold();
 
 			if(nNxtButtonPressed == 1)
 				rotate(false, -1, 0);
@@ -241,17 +235,16 @@ task main()
 				rotate(true, -1, 0);
 
 			while(nNxtButtonPressed == -1){}
-			//buttonHold();
 		}
 		randomShot = false;
 	}
 
-	while(nNxtButtonPressed != -1){}
+	while(nNxtButtonPressed != -1){} // If button being held down, don't do anything
 
 	do
 	{
 		eraseDisplay();
-		time = touchTimer();
+		time = touchTimer();	// Set shooting time interval via touch sensor
 		displayString(3, "Press Orange");
 		displayString(4, "button to go on");
 		displayString(5, "Else press any");
@@ -261,7 +254,7 @@ task main()
 		while(nNxtButtonPressed == -1){}
 	} while(nNxtButtonPressed != 3);
 
-	while(nNxtButtonPressed != -1){}
+	while(nNxtButtonPressed != -1){} // If button being held down, don't do anything
 
 	eraseDisplay();
 	displayString(1, "Select # of shots:");
@@ -286,7 +279,7 @@ task main()
 		}
 	}
 
-	while(nNxtButtonPressed == 3){}; // <--- What's this for??
+	while(nNxtButtonPressed == 3){}; 
 
 	eraseDisplay();
 	displayString(0, "Start by");
@@ -298,28 +291,27 @@ task main()
 
 	if(totalShots-shotsShot){
 
-	while(!eStop)
-	{
-		time1[T1] = 0;
+		while(!eStop)
+		{
+			time1[T1] = 0;
 
-		if(randomShot)
-			setLocation(time);
+			if(randomShot)		// If random location setting
+				setLocation(time);
 
-		loadBall();
+			loadBall();
 
-		while(time1[T1] < time - 200){}
+			while(time1[T1] < time - 200){}	// Wait for time interval to be reached
 
-		shootBall(0, -170, -60);
-		shotsShot++;
+			shootBall(0, -170, -60);
+			shotsShot++;
 
-		//eStop = displayData(time, shotsShot, totalShots);
-		displayData(time, shotsShot, totalShots);
-		eStop = checkStop(totalShots-shotsShot);
-	}
+			displayData(time, shotsShot, totalShots);
+			eStop = checkStop(totalShots-shotsShot);
+		}
 
-	displayString(4, "Press any button");
-	displayString(5, "to end program");
-	buttonHold(); 	//Let user see displayData stuff
-	eraseDisplay();
+		displayString(4, "Press any button");
+		displayString(5, "to end program");
+		buttonHold(); 	//Let user see displayData stuff
+		eraseDisplay();
 	}
 }
